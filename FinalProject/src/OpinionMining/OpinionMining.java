6606c79e6be1c8291d95f70361113ffbbec1e29a
park-seung-chul <fox9101@naver.com>
2016-01-07 01:00:02 +0900

    댓글의 관한 긍부정 처리 DaoTravlePlace IDao ModelComment ModelCommentList ModelInformation detailPage.jsp comment_save.jsp만 건들임

--- FinalProject/src/OpinionMining/OpinionMining.java
+++ FinalProject/src/OpinionMining/OpinionMining.java
@@ -6,26 +6,27 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class OpinionMining {
    private Map<String, Double> dictionary;
-   private double sentiment;
+   private double positiveSentiment, negativeSentiment;
    
    public OpinionMining() {
       dictionary = new HashMap<String, Double>();
    }
 
-   public void create(String dictionaryPath) throws IOException {
+   public void create(File diction) throws IOException {
       BufferedReader file = null;
       try {
-         file = new BufferedReader(new FileReader(dictionaryPath));
+         file = new BufferedReader(new FileReader(diction));
 
          String line;
          while ((line = file.readLine()) != null) {
             if (!line.trim().startsWith("\\s")) {
                String[] data = line.split(" ");
                String sentiword = data[0];
-                Double score = Double.parseDouble(data[1]);
+               Double score = Double.parseDouble(data[1]);
                
                dictionary.put(sentiword, score);
             }
@@ -39,10 +40,10 @@ public class OpinionMining {
       }
    }
    
-   public void calculate(String testPath) throws IOException {
+   public void calculateFile(String testPath) throws IOException {
       String line = null;
-      double sum = 0.0;
-      int count = 0;
+      double sumNegative = 0.0, sumPositive = 0.0;
+      int countNegative = 0, countPositive = 0;
       BufferedReader file = null;
 
       try {
@@ -60,19 +61,31 @@ public class OpinionMining {
                      if (dictionary.containsKey(tokens[x].substring(y, y+2))){
                         System.out.println(tokens[x].substring(y, y+2));
                         System.out.println(dictionary.get(tokens[x].substring(y, y+2)));
-                        count++;
-                        sum += dictionary.get(tokens[x].substring(y, y+2));
+                        if(dictionary.get(tokens[x].substring(y, y+2)) > 0)
+                        {
+                            sumPositive += dictionary.get(tokens[x].substring(y, y+2));
+                            countPositive++;
+                        }
+                        else
+                        {
+                            sumNegative += dictionary.get(tokens[x].substring(y, y+2));
+                            countNegative++;
+                        }
                         break;
                      }
-
                   }                  
                }
             }
          }
-         if(count != 0)
-            setSentiment((double)Math.round(sum / count * Math.pow(10.0, 3)) / Math.pow(10.0, 3));
+         if((countPositive != 0))
+             setPositiveSentiment((double)Math.round(sumPositive / countPositive * Math.pow(10.0, 3)) / Math.pow(10.0, 3));
          else
-             setSentiment(0);
+             setPositiveSentiment(0);
+
+         if((countNegative != 0))
+             setNegativeSentiment((double)Math.round(sumNegative / countNegative * Math.pow(10.0, 3)) / Math.pow(10.0, 3));
+         else
+             setNegativeSentiment(0);       
      
    //      if(count == 0){
    //         System.out.println("���� : " + getSentiment()*100 + "%");
@@ -89,14 +102,62 @@ public class OpinionMining {
             file.close();
          }
       }
-   }
-   
-   public double getSentiment()
+   }   
+   public void calculatePnn(String test) {
+       double sumNegative = 0.0, sumPositive = 0.0;
+       int countNegative = 0, countPositive = 0;
+       
+       StringTokenizer line = new StringTokenizer(test , " " );
+          
+       while (line.hasMoreTokens()) {
+          String tokens = line.nextToken();
+          if(tokens.length() >= 2){
+//           System.out.println(tokens[x]);
+             for (int y=0; y<tokens.length()-1;y++){
+//              System.out.println(tokens[x].substring(y, y+2));
+
+                if (dictionary.containsKey(tokens.substring(y, y+2))){
+//                   System.out.println(tokens.substring(y, y+2));
+//                   System.out.println(dictionary.get(tokens.substring(y, y+2)));
+                   if(dictionary.get(tokens.substring(y, y+2)) > 0)
+                   {
+                       sumPositive += dictionary.get(tokens.substring(y, y+2));
+                       countPositive++;
+                   }
+                   else
+                   {
+                       sumNegative += dictionary.get(tokens.substring(y, y+2));
+                       countNegative++;
+                   }
+                   break;
+                }
+             }
+          }
+       }
+       if((countPositive != 0))
+           setPositiveSentiment((double)Math.round(sumPositive / countPositive * Math.pow(10.0, 3)) / Math.pow(10.0, 3));
+       else
+           setPositiveSentiment(0);
+
+       if((countNegative != 0))
+           setNegativeSentiment((double)Math.round(sumNegative / countNegative * Math.pow(10.0, 3)) / Math.pow(10.0, 3));
+       else
+           setNegativeSentiment(0);       
+    }
+   public double getPositiveSentiment()
    {
-       return sentiment;
+       return positiveSentiment;
    }
-   public void setSentiment(double sentiment)
+   public void setPositiveSentiment(double positiveSentiment)
    {
-       this.sentiment = sentiment;
+       this.positiveSentiment = positiveSentiment;
+   }
+   public double getNegativeSentiment()
+   {
+       return negativeSentiment;
+   }
+   public void setNegativeSentiment(double negativeSentiment)
+   {
+       this.negativeSentiment = negativeSentiment;
    }
 }
